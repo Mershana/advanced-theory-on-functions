2454fb43e8cb62f5fd6b3a0206059072c57b9130
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-01 19:21:30 +0300

    05. глобальные переменные и параметры функций

---
+++ 05. глобальные переменные и параметры функций/js/app.js
@@ -0,0 +1,67 @@
+'use strict'
+
+// Глобальные переменные и параметры функций в JavaScript
+
+
+/* №1⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* function func(localNum) {
+	console.log(localNum);
+}
+func(1); */ //1
+
+/* №2⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* function func(localNum) {
+	console.log(localNum);
+}
+
+let num = 1;
+func(num); */ // 1
+
+/* №3⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* function func(localNum) {
+	console.log(localNum);
+}
+let num = 1;
+func(num); //1
+num = 2; */
+
+/* №4⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func(localNum) {
+	console.log(localNum);
+}
+num = 2;
+func(num);  *///
+
+/* №5⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль:
+ */
+/* function func(localNum) {
+	localNum = 2;
+}
+let num = 1;
+func(num);
+console.log(num); */ // 1
+
+/* №6⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* function func(localNum) {
+	num = 2;
+}
+let num = 1;
+func(num); 
+console.log(num); */ // 2
+
+/* №7⊗jsPmFAGVP
+Определите, не запуская код, что выведется в консоль: */
+/* function func(localNum) {
+	let num = 2;
+}
+
+let num = 1; 
+func(num);
+console.log(num); */ // 1 
