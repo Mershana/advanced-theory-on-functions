817d0e8042e6ebab7aa776a62f9deb2714f9fad8
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-01 19:20:39 +0300

    02. локальные переменные

---
+++ 02. локальные переменные/js/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Локальные переменные в функциях в JavaScript
+/* №1⊗jsPmFALV
+Определите, не запуская код, что выведется в консоль: */
+
+/* function func() {
+	let num = 5;
+	return num;
+}
+console.log(num); */ //ошибка 
+
+/* №2⊗jsPmFALV
+Определите, не запуская код, что выведется в консоль: */
+/* function func() {
+	let num = 5;
+	return num;
+}
+
+console.log(func()); */ //5
