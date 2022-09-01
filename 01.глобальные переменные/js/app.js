d2fb3025baa43d6b54442bc4eb37ca8b8ae535ec
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-01 19:20:20 +0300

    01.глобальные переменные

---
+++ 01.глобальные переменные/js/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// Глобальные переменные в функциях в JavaScript
+
+/* №1⊗jsPmFAGV
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;	
+function func() {
+	console.log(num);
+}
+func(); */ //1
+
+/* №2⊗jsPmFAGV
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	console.log(num);
+}
+num = 2;
+func();  *///2
+
+/* №3⊗jsPmFAGV
+Определите, не запуская код, что выведется в консоль: */
+
+/* function func() {
+	console.log(num);
+}
+let num;
+num = 1;
+func(); //1 
+
+num = 2;
+func(); //2 */
+
