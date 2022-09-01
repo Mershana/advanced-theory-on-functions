ef374878f07983d12357c6323fe6bd8c043a9a43
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-01 19:21:12 +0300

    04. изменение глобальных переменных

---
+++ 04. изменение глобальных переменных/js/app.js
@@ -0,0 +1,82 @@
+'use strict'
+
+// Изменение глобальных переменных в JavaScript
+/* №1⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	num = 2;
+}
+func();
+console.log(num); */ //2
+
+/* №2⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	num = 2;
+}
+console.log(num);  */// 1
+
+/* №3⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	let num = 2;
+}
+func();
+
+console.log(num); // 1 */
+
+/* №4⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	let num = 2;
+}
+
+console.log(num);
+func(); // 1 */
+
+/* №5⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	num = 2;
+}
+
+console.log(num);
+func(); // 1 */
+
+/* №6⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	num++;
+}
+func();
+func();
+func();
+console.log(num); // 4 */
+
+/* №7⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* function func() {
+	num = 2;
+}
+
+let num = 1;
+console.log(num); // 1
+func();  */
+
+/* №8⊗jsPmFAGVCh
+Определите, не запуская код, что выведется в консоль: */
+/* function func() {
+	num = 2;
+}
+
+let num = 1;
+func();
+console.log(num); //2 */
+
+
