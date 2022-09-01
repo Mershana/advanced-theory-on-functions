e7f30a663673f6e0fc04b71440e7830c5a99559d
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-01 19:20:54 +0300

    03. совпадение имен переменных

---
+++ 03. совпадение имен переменных/js/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Одинаковые имена переменных в функциях в JavaScript
+/* №1⊗jsPmFAVNC
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	num = 2;
+}
+func();
+console.log(num); */ // 2
+
+/* №2⊗jsPmFAVNC
+Определите, не запуская код, что выведется в консоль: */
+/* let num = 1;
+function func() {
+	let num = 2;
+}
+func(); //2
+console.log(num); */ //1  глобальная переменная не поменялась 
+
