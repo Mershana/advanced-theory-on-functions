00b7bc916106bc562d976414ac6e8516c84ca967
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-02 14:55:07 +0300

    06. совпадение имен с параметрами

---
+++ 06. совпадение имен с параметрами/js/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// Совпадение имен с параметрами в JavaScript
+
+/* №1⊗jsPmFAPNC
+Определите, не запуская код, что выведется в консоль: */
+/* function func(num) {
+	num = 2;
+}
+
+let num = 1;
+func(num);
+console.log(num); */ //1 
+
+/* №2⊗jsPmFAPNC
+Определите, не запуская код, что выведется в консоль: */
+
+/* function func() {
+	num = 2;
+}
+
+let num = 1;
+func();
+console.log(num); // 2 */
+
+/* №3⊗jsPmFAPNC
+Определите, не запуская код, что выведется в консоль: */
+/* function func() {
+	let num = 2;
+}
+
+let num = 1;
+func();
+console.log(num); */ //1
