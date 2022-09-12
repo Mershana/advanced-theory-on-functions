d479eda3d8b4d05e01f41efd2ac7ed56dd827b37
Mershana <102474997+Mershana@users.noreply.github.com>
2022-09-12 12:33:37 +0300

    016. вложенные функции

---
+++ 016. вложенные функции/js/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Вложенные функции в JavaScript
+
+/* №1⊗jsPmFANF
+Сделайте функцию func, которая параметрами будет принимать два числа, а возвращать сумму квадрата первого числа с кубом второго числа. Сделайте для этого вспомогательную функцию square, возводящую число в квадрат, и вспомогательную функцию cube, возводящую число в куб. */
+/* function func(num1, num2){
+	function getSquare(num){
+		return num * num; 
+	}
+	function getCube(num){
+		return num * num * num
+	}
+	
+	return getSquare(num1) + getCube(num2);
+}
+console.log(func(2, 3));  *///31
